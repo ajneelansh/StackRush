11e8e3d02ef1667387fad5f3c5a41869a272d32a
ajneelansh <ajneelansh@gmail.com>
2025-04-18 23:20:34 +0530

    Added FAQs section in landing page

---
+++ Frontend/app/layout.tsx
@@ -0,0 +1,30 @@
+import type { Metadata } from "next";
+import {DM_Sans} from "next/font/google";
+import clsx from "clsx";
+import "./globals.css";
+
+const dmSans= DM_Sans({
+  variable: "--font-DM_Sans",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "CodeHurdle",
+  description: "Level-Up your Problem-Solving",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={clsx(dmSans.className, "antialiased")}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
